5ae7b46ddfcc76477a8725719f9ade4999afc931
João Vitor Farias <joaofarias_16@hotmail.com>
2024-11-05 18:27:21 -0300

    portfolio added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
+// Download do Currículo
 function downloadFile() {
   const link = document.createElement("a");
   link.href = "Documentos/curriculo.pdf";
   link.download = "Currículo.pdf";
   link.click();
 }
+
+// Portfolio Hover nas imagens
+const containers = document.querySelectorAll(".img-hover");
+
+containers.forEach((container) => {
+  const img = container.querySelector(".img-project");
+
+  // Cria o elemento de + e adiciona ao container
+  const plusIcon = document.createElement("a");
+  plusIcon.href = container.getAttribute("data-link");
+  plusIcon.classList.add("plus-icon");
+  plusIcon.textContent = "+";
+  plusIcon.target = "_blank";
+  container.appendChild(plusIcon);
+
+  // Adiciona o evento de hover
+  container.addEventListener("mouseenter", () => {
+    img.style.filter = "brightness(0.3)";
+    plusIcon.style.display = "block";
+     container.style.boxShadow = "0 0 3px #1e90ff"
+     container.style.borderRadius = "10px"
+  });
+
+  container.addEventListener("mouseleave", () => {
+    img.style.filter = "brightness(1)";
+    plusIcon.style.display = "none";
+     container.style.boxShadow = "none"
+  });
+});
